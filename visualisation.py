f12a50c9fb959dbba14f674aafb7eaa15e9ad78b
mousey <91616572+anastasiasenyk@users.noreply.github.com>
2022-02-21 13:07:01 +0200

    visualisation2.0

--- visualisation.py
+++ visualisation.py
@@ -3,19 +3,32 @@ import pandas as pd
 import numpy as np
 
 
-def read_data():
+def read_data(max_vertex):
+    """
+
+    :param max_vertex:
+    :return:
+    """
     file = pd.read_csv('dataset.csv')
-    result_kruskal = {}
-    result_prim = {}
+    result_kruskal = []
+    result_prim = []
+    possib_repeat = int(100/int(max_vertex//5))
     for index, row in file.iterrows():
         row = [float(x) for x in list(row)]
-        if (row[3], row[4]) not in list(result_prim.keys()):
-            result_prim[(row[3], row[4])] = row[2:]
-            result_kruskal[(row[3], row[4])] = [row[1]] + row[3:]
-    return list(result_kruskal.values()), list(result_prim.values())
+        if row[3] > max_vertex or row[4] * 100 % possib_repeat != 0:
+            continue
+        result_prim.append(row[2:])
+        result_kruskal.append([row[1]] + row[3:])
+    return result_kruskal, result_prim
 
 
 def visual(data1, data2):
+    """
+
+    :param data1:
+    :param data2:
+    :return:
+    """
     data1 = np.array(data1)
     data2 = np.array(data2)
     fig = plt.figure(figsize=(10, 10))
@@ -26,8 +39,17 @@ def visual(data1, data2):
     ax.set_ylabel("Num of nodes (N)")
     ax.set_zlabel("Time (s)")
     ax.legend(loc="best")
-    ax.view_init(15, 200)
+    ax.view_init(15, 225)
     plt.show()
 
 
-visual(*read_data())
+if __name__ == "__main__":
+    while True:
+        print('Enter number of nodes (5<=nodes<=500)')
+        num = input('>>> ')
+        if num.isdigit() and 5 <= int(num) <= 500:
+            num = int(num)
+            break
+        else:
+            print('number (type: int)')
+    visual(*read_data(num))
